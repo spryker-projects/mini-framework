d1494636ce20f4234097633e1ebcfdd3cbd5bad3
Ahmed Sabaa <ahmed.sabaa@spryker.com>
2022-02-13 12:45:58 +0100

    added last 2 controller actions

--- src/Pyz/Glue/TestApi/Controller/TestResourceController.php
+++ src/Pyz/Glue/TestApi/Controller/TestResourceController.php
@@ -6,7 +6,9 @@ use Generated\Shared\Transfer\GlueErrorTransfer;
 use Generated\Shared\Transfer\GlueRequestTransfer;
 use Generated\Shared\Transfer\GlueResourceTransfer;
 use Generated\Shared\Transfer\GlueResponseTransfer;
+use Generated\Shared\Transfer\TestCollectionDeleteCriteriaTransfer;
 use Generated\Shared\Transfer\TestCollectionRequestTransfer;
+use Generated\Shared\Transfer\TestCollectionResponseTransfer;
 use Generated\Shared\Transfer\TestConditionsTransfer;
 use Generated\Shared\Transfer\TestCriteriaTransfer;
 use Generated\Shared\Transfer\TestTransfer;
@@ -57,18 +59,78 @@ class TestResourceController extends AbstractBackendApiController
 
     public function getAction(string $id): GlueResponseTransfer
     {
-        $test = '';
+        $testCriteriaTransfer = new TestCriteriaTransfer();
+        $testCriteriaTransfer->setTestConditions((new TestConditionsTransfer())->addTestId($id));
+
+        $testCollectionTransfer = $this->getFactory()->getTestFacade()->getTestCollection($testCriteriaTransfer);
+
+        $glueResponseTransfer = new GlueResponseTransfer();
+        if((array)$testCollectionTransfer->getTests()) {
+            $resourceTransfer = new GlueResourceTransfer();
+            $resourceTransfer->setAttributes($testCollectionTransfer->getTests()[0]);
+            $resourceTransfer->setId($testCollectionTransfer->getTests()[0]->getId());
+            $resourceTransfer->setType('test');
+
+            $glueResponseTransfer->addResource($resourceTransfer);
+            return $glueResponseTransfer;
+        }
+
+        $glueResponseTransfer->setStatus(404)->addError((new GlueErrorTransfer())->setMessage('not found'));
+
+        return $glueResponseTransfer;
     }
 
-    public function postAction(TestTransfer $testTransfer, GlueRequestTransfer $glueRequestTransfer): GlueResponseTransfer
+    public function postAction(TestTransfer $testTransfer): GlueResponseTransfer
     {
         $testCollectionRequestTransfer = new TestCollectionRequestTransfer();
         $testCollectionRequestTransfer->addTest($testTransfer)->setIsTransactional(false);
 
-        $testCollectionResponseTransfer = $this->getFactory()->getTestFacade()->createTestCollection($testCollectionRequestTransfer);
+        return $this->returnSaveResponse(
+            $this->getFactory()->getTestFacade()->createTestCollection($testCollectionRequestTransfer)
+        );
+    }
 
+    public function patchAction(TestTransfer $testTransfer): GlueResponseTransfer
+    {
+        $testCollectionRequestTransfer = new TestCollectionRequestTransfer();
+        $testCollectionRequestTransfer->addTest($testTransfer)->setIsTransactional(false);
+
+
+        return $this->returnSaveResponse(
+            $this->getFactory()->getTestFacade()->updateTestCollection($testCollectionRequestTransfer)
+        );
+    }
+
+    public function deleteAction(string $id): GlueResponseTransfer
+    {
+        $testCollectionDeleteCriteriaTransfer = new TestCollectionDeleteCriteriaTransfer();
+        $testCollectionDeleteCriteriaTransfer->setIsTransactional(false)->addIdTest($id);
+
+        $testCollectionResponseTransfer =
+            $this->getFactory()->getTestFacade()->deleteTestCollection($testCollectionDeleteCriteriaTransfer);
+
+        if(!(array)$testCollectionResponseTransfer->getTests())
+        {
+            $glueResponseTransfer = new GlueResponseTransfer();
+            $glueResponseTransfer->setStatus(404)->addError((new GlueErrorTransfer())->setMessage('not found'));
+
+            return $glueResponseTransfer;
+        }
+
+        return $this->returnSaveResponse(
+            $this->getFactory()->getTestFacade()->deleteTestCollection($testCollectionDeleteCriteriaTransfer)
+        );
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\TestCollectionResponseTransfer $testCollectionResponseTransfer
+     *
+     * @return GlueResponseTransfer
+     */
+    protected function returnSaveResponse(TestCollectionResponseTransfer $testCollectionResponseTransfer): GlueResponseTransfer
+    {
         $glueResponseTransfer = new GlueResponseTransfer();
-        if(!(array)$testCollectionResponseTransfer->getErrors()) {
+        if (!(array)$testCollectionResponseTransfer->getTests()) {
             $resourceTransfer = new GlueResourceTransfer();
             $resourceTransfer->setAttributes($testCollectionResponseTransfer->getTests()->offsetGet(0));
             $resourceTransfer->setId($testCollectionResponseTransfer->getTests()->offsetGet(0)->getId());
@@ -79,20 +141,10 @@ class TestResourceController extends AbstractBackendApiController
             return $glueResponseTransfer;
         }
 
-        foreach($testCollectionResponseTransfer->getErrors() as $error) {
+        foreach ($testCollectionResponseTransfer->getErrors() as $error) {
             $glueResponseTransfer->addError((new GlueErrorTransfer())->setMessage($error->getMessage()));
         }
 
         return $glueResponseTransfer;
-    }
-
-    public function patchAction(TestTransfer $testTransfer): GlueResponseTransfer
-    {
-        $test = '';
-    }
-
-    public function deleteAction(string $id): GlueResponseTransfer
-    {
-        $test = '';
     }
 }
