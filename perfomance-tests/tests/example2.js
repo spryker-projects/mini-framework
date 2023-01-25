e6b769644b74216da4507000e584945faf88821a
Patrik Rozgonyi <patrik.rozgonyi@spryker.com>
2023-01-25 11:34:53 +0100

    updated examples with standalone and scenarios too

---
+++ perfomance-tests/tests/example2.js
@@ -0,0 +1,17 @@
+import { group, check } from "k6";
+import { logError, loadOptions, loadServiceConfig } from "../lib/utils.js";
+import {sendGetRequestWithHttpAuth, sendPostRequestWithHttpAuth} from "../lib/http.js";
+
+export let options = loadOptions('default-standalone');
+export let serviceConfig = loadServiceConfig('b2c');
+
+export function setup() { return serviceConfig; }
+
+export default function(data) {
+
+    group("Check Homepage", function() {
+        let response = sendGetRequestWithHttpAuth(data.baseUrl);
+        check(response, { "is success status code": 200}) || logError(response);
+    });
+
+};
--- perfomance-tests/tests/scnenarios-example.js
+++ perfomance-tests/tests/scnenarios-example.js
@@ -1,15 +1,15 @@
-﻿import { loadOptions, loadServiceConfig } from "./lib/utils.js";
+﻿import { loadOptions, loadServiceConfig } from "../lib/utils.js";
 import {textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
-export let options = loadOptions('default');
+export let options = loadOptions('default-scenarios');
 export let serviceConfig = loadServiceConfig('b2c');
 
 export function setup() { return serviceConfig; }
 
-export { default as example } from './tests/example.js';
+export { default as example } from './example.js';
 
 export default function (data) {
- 
+
 };
 
 export function handleSummary(data) {
