152eeeb3c3ba17fb0379d641469f89aa8edab567
Patrik Rozgonyi <patrik.rozgonyi@spryker.com>
2023-01-25 10:56:46 +0100

    replace http credentials from env vars to services config

--- perfomance-tests/lib/http.js
+++ perfomance-tests/lib/http.js
@@ -1,15 +1,14 @@
 import http from 'k6/http';
 import encoding from 'k6/encoding';
 
-export function sendGetRequestWithHttpAuth(url) {
-    if (!__ENV.TEST_MACHINE_HTTPAUTH_USERNAME || !__ENV.TEST_MACHINE_HTTPAUTH_PASSWORD) {
+export function sendGetRequestWithHttpAuth(url, authenticationOptions) {
+    if (!authenticationOptions.username || !authenticationOptions.password) {
         fail('TEST_MACHINE_HTTPAUTH_USERNAME and TEST_MACHINE_HTTPAUTH_PASSWORD must to be specified to send GET requests with HTTP authentication');
     }
 
-
     return http.get(url, {
         headers: {
-          'Authorization': `Basic ${encoding.b64encode(`${__ENV.TEST_MACHINE_HTTPAUTH_USERNAME}:${__ENV.TEST_MACHINE_HTTPAUTH_PASSWORD}`)}`,
+          'Authorization': `Basic ${encoding.b64encode(`${authenticationOptions.username}:${authenticationOptions.password}`)}`,
         }
     });
 }
--- perfomance-tests/tests/example.js
+++ perfomance-tests/tests/example.js
@@ -4,7 +4,7 @@ import { sendGetRequestWithHttpAuth } from "../lib/http.js";
 
 export default function (data) {
     group("Example tests", function() {
-        let response = sendGetRequestWithHttpAuth(data.baseUrl);
+        let response = sendGetRequestWithHttpAuth(data.baseUrl, {"username": data.username, "password": data.password});
         check(response, { "is success status code": 200}) || logError(response);
     });
 
