0c5645a4eccb5fa14525058392c403015d9be6bd
Patrik Rozgonyi <patrik.rozgonyi@spryker.com>
2023-01-25 21:10:33 +0100

    fix issues caused by refactoring

--- perfomance-tests/tests/example-scenario-test.js
+++ perfomance-tests/tests/example-scenario-test.js
@@ -1,16 +1,16 @@
 import { group, check } from "k6";
 import { logError, loadOptions, loadEnvironmentConfig } from "../lib/utils.js";
-import {sendGetRequestWithHttpAuth, sendPostRequestWithHttpAuth} from "../lib/http.js";
+import {sendGetRequestWithHttpAuth} from "../lib/http.js";
 
 export let options = loadOptions('default-standalone');
-export let serviceConfig = loadEnvironmentConfig('b2c');
+export let environmentConfig = loadEnvironmentConfig('b2c');
 
-export function setup() { return serviceConfig; }
+export function setup() { return environmentConfig; }
 
 export default function(data) {
 
     group("Check Homepage", function() {
-        let response = sendGetRequestWithHttpAuth(data.baseUrl, {"username": data.username, "password": data.password});
+        let response = sendGetRequestWithHttpAuth(data.baseUrl, {"username": data.httpauth_username, "password": data.httpauth_password});
         check(response, { "is success status code": 200}) || logError(response);
     });
 
--- perfomance-tests/tests/simple-example.js
+++ perfomance-tests/tests/simple-example.js
@@ -1,10 +1,15 @@
 import { group, check } from "k6";
-import { logError } from "../lib/utils.js";
+import { loadOptions, loadEnvironmentConfig, logError } from "../lib/utils.js";
 import { sendGetRequestWithHttpAuth } from "../lib/http.js";
+
+export let options = loadOptions('default-standalone');
+export let environmentConfig = loadEnvironmentConfig('b2c');
+
+export function setup() { return environmentConfig; }
 
 export default function (data) {
     group("Example tests", function() {
-        let response = sendGetRequestWithHttpAuth(data.baseUrl, {"username": data.username, "password": data.password});
+        let response = sendGetRequestWithHttpAuth(data.baseUrl, {"username": data.httpauth_username, "password": data.httpauth_password});
         check(response, { "is success status code": 200}) || logError(response);
     });
 
