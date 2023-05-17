e2118ab06f7337167b819311f6f81f4b958a3a5e
Bohdan Turchyk <bohdan.turchyk@spryker.com>
2023-05-17 12:52:55 +0300

    Backport of PBC-1528: Fixed jenkins setup files - scheduler jobs weren't set up automatically

--- config/Zed/cronjobs/jenkins.php
+++ config/Zed/cronjobs/jenkins.php
@@ -1,9 +1,22 @@
 <?php
+
+/*
+ * Snippet to add on commands that we wnat to send logs to Cloudwatch.
+ * Issue and solution reported on https://spryker.atlassian.net/browse/PBC-1349.
+ */
+$sendOutputToJenkinsCommandSnippet = ' 2>&1 | tr "\n" "\0" | xargs -0 printf "${JOB_NAME} ${BUILD_NUMBER} %s\n" | tee -a /proc/1/fd/1';
+
+$stores = require(APPLICATION_ROOT_DIR . '/config/Shared/stores.php');
+
+$allStores = array_keys($stores);
+
+$consolePath = 'vendor/bin/console';
+
 $jobs[] = [
     'name' => 'test',
-    'command' => '$PHP_BIN vendor/bin/console help -q',
+    'command' => '$PHP_BIN ' . $consolePath . ' help -q',
     'schedule' => '*/1 * * * *',
     'enable' => true,
     'run_on_non_production' => true,
-    'stores' => ['DE'],
+    'stores' => $allStores,
 ];
