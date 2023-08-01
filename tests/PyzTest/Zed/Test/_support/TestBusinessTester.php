41e5068315f77a5d3ca37af2ed2d4bb1179427b3
stereomon <rene.klatt@spryker.com>
2023-08-01 13:33:18 +0200

    Fixed CS issues

--- tests/PyzTest/Zed/Test/_support/TestBusinessTester.php
+++ tests/PyzTest/Zed/Test/_support/TestBusinessTester.php
@@ -23,7 +23,7 @@ use Codeception\Actor;
  * @method void comment($description)
  * @method void pause()
  *
- * @SuppressWarnings(PHPMD)
+ * @SuppressWarnings(\PyzTest\Zed\Test\PHPMD)
  *
  * @method \Pyz\Zed\Test\Business\TestFacadeInterface getFacade()
  */
