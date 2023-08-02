5c2fcfa054c8fb9c63072c2b170d8df017b4644d
stereomon <rene.klatt@spryker.com>
2023-08-02 10:28:37 +0200

    Added Propel files.

---
+++ src/Orm/Zed/Test/Persistence/SpyTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\Test\Persistence;
+
+use Orm\Zed\Test\Persistence\Base\SpyTest as BaseSpyTest;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_test' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyTest extends BaseSpyTest
+{
+
+}
